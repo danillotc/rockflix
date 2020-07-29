e0e8f8963c2511d76885bf48eb219e63a40f4365
danillotc <danillotc@hotmail.com>
2020-07-29 18:39:37 -0300

    fixed responsivity

--- src/components/FormField/styles.js
+++ src/components/FormField/styles.js
@@ -3,17 +3,23 @@ import styled from 'styled-components';
 export const FormFieldInput = styled.input`
     padding: .5em;
     margin-left: 1em;
-    margin-right: 7em;
     margin-bottom: 2em;
+    margin-right: 7em;
     color: var(--white);
     background-color: var(--black);
     border-color: var(--primary);
     border-radius: 4px;
+    
+    @media (max-width: 800px) {
+        & {
+            margin-right: 1em;
+            display: flex;
+        }
+    }
 `; 
 
 export const FormFieldTextArea = styled.textarea`
     display: flex;
-    width: 35em;
     padding: .5em;
     margin-left: 1em;
     margin-bottom: 2em;
@@ -21,4 +27,11 @@ export const FormFieldTextArea = styled.textarea`
     background-color: var(--black);
     border-color: var(--primary);
     border-radius: 4px;
+    
+    width: 35em;
+    @media (max-width: 800px) {
+        & {
+            width: 18em;
+        }
+    }
 `; 
