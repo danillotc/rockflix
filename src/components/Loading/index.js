5b830f6e4d5447dbf2b4882007efb680e878afc4
danillotc <danillotc@hotmail.com>
2020-08-10 17:04:35 -0300

    changed loading page

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
 
-const Loading = styled.div`
+export const Loading = styled.div`
     height: 100vh;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    justify-content: space-evenly;
     align-items: center;
     font-size: 3em;
     color: var(--primary);
     animation-name: gradienteDeCor;
-    animation-duration: .8s;
+    animation-duration: 1s;
     animation-iteration-count: infinite;
     animation-direction: alternate;
     animation-timing-function: ease-in;
@@ -24,4 +25,20 @@ const Loading = styled.div`
 
 `;
 
-export default Loading;
+export const LoadingLogo = styled.img`
+    scale: 1.5;
+    animation-name: animaLogo;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+    animation-direction: alternate;
+    animation-timing-function: ease-in;
+
+    @keyframes animaLogo {
+        0% {
+            opacity: 0%;
+        }
+        100% {
+            opacity: 100%;
+        }
+    }
+`;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,8 @@ import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
 import categoriasRepository from '../../repositories/categories';
 import PageDefault from '../../components/pageDefault';
-import Loading from '../../components/Loading';
+import { Loading, LoadingLogo } from '../../components/Loading';
+import loadingimg from '../../assets/img/loadingimg.png';
 
 function Home() {
   const [dadosIniciais, setDadosIniciais] = useState([]);
@@ -22,7 +23,14 @@ function Home() {
   return (
     <PageDefault paddingHome={0}>
 
-      {dadosIniciais.length === 0 && (<Loading>Carregando...</Loading>)}
+      {dadosIniciais.length === 0 && (
+        <>
+          <Loading>
+            Carregando...
+            <LoadingLogo src={loadingimg} alt="Loading logo" />
+          </Loading>
+        </>
+      )}
 
       {dadosIniciais.map((categoria, indice) => (
         indice === 0
